1caf217368fc73fe57070a86195c6692dd98f3da
scape76 <danyaolekhq@gmail.com>
2023-07-11 12:14:47 +0300

    show author tweets on page

--- src/app/(home)/[userId]/page.tsx
+++ src/app/(home)/[userId]/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/layout/Header";
 import { db } from "@/db";
-import { users } from "@/db/schema";
+import { users, tweets } from "@/db/schema";
 import { eq } from "drizzle-orm";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import * as React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
 import { getUserLiterals, updateImageUrl } from "@/lib/utils";
+import TweetItem from "@/components/TweetItem";
+import { getCurrentUser } from "@/lib/session";
 
 interface UserPageProps {
   params: {
@@ -14,26 +16,39 @@ interface UserPageProps {
 }
 
 const UserPage: React.FC<UserPageProps> = async ({ params }) => {
-  const user = await db.query.users.findFirst({
+  const user = await getCurrentUser();
+
+  if (!user) redirect("/login");
+
+  const author = await db.query.users.findFirst({
     where: eq(users.id, params.userId),
   });
 
-  if (!user) notFound();
+  if (!author) notFound();
 
-  console.log(user.image);
+  const authorTweets = await db.query.tweets.findMany({
+    where: eq(tweets.authorId, author.id),
+    with: {
+      author: true,
+    },
+  });
 
   return (
     <div>
-      <Header title={user.name ?? ""} />
+      <Header title={author.name ?? ""} />
       <div className="relative h-36 bg-accent">
         <Avatar className="absolute bottom-0 left-0 -mb-[64px] ml-4 h-32 w-32 rounded-full border-4 border-background">
-          <AvatarImage src={updateImageUrl(user.image ?? "", 216)} />
-          <AvatarFallback>{getUserLiterals(user.name)}</AvatarFallback>
+          <AvatarImage src={updateImageUrl(author.image ?? "", 216)} />
+          <AvatarFallback>{getUserLiterals(author.name)}</AvatarFallback>
         </Avatar>
       </div>
       <div className="h-[64px]" />
       <hr className="w-full border-b border-border" />
-      {user.email} page
+      <div>
+        {authorTweets.map((t) => (
+          <TweetItem tweet={t} user={user} />
+        ))}
+      </div>
     </div>
   );
 };
